28a4f8d61280a34a2fd0985cae569acac830a1eb
Murilo Henrique <murilohns@gmail.com>
2018-05-28 08:51:59 -0300

    add some categories

--- nodejs-developer/noticias.js
+++ nodejs-developer/noticias.js
@@ -1,7 +1,27 @@
 var http = require('http');
 
-var server = http.createServer( function(req, res) { 
-    res.end("<html><body>Portal de notícias</body></html>");
+var server = http.createServer( function(req, res) {
+
+    var category = req.url;
+    var responseText = "";
+
+    if(category == "/technology") {
+        responseText = "Notícias de Tecnologia";
+    }
+
+    if(category == "/fashion") {
+        responseText = "Notícias de Moda";
+    }
+
+    if(category == "/beauty") {
+        responseText = "Notícias de beleza"
+    }
+
+    if(category == "/") {
+        responseText = "Página de notícias"
+    }
+
+    res.end("<html><body>" + responseText + "</body></html>");
 })
 
 server.listen(3000);
