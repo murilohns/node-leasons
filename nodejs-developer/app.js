5002b14c0f86af8421edcfd5482f2f859c9de616
Murilo Henrique <murilohns@gmail.com>
2018-05-29 08:46:38 -0300

    add support to ejs

--- nodejs-developer/app.js
+++ nodejs-developer/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.send("<html><body>Portal de noticias</body></html>");
 });
 
 app.get('/technology', (req, res) => {
-    res.send("<html><body>Notícias de tecnologia</body></html>");
+    res.render("category/technology");
 });
 
 app.get('/fashion', (req, res) => {
     res.send("<html><body>Notícias de Moda</body></html>");
 });
-
 
 app.get('/beauty', (req, res) => {
     res.send("<html><body>Notícias de beleza</body></html>");
